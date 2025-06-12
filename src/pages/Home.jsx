d8ea29cf60b34b10376e4be6b2125c2b2f914b72
priyan1995 <priyan.abewickrama@ideahub.lk>
2025-06-12 19:34:10 +0530

    home page development

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import HomeBannerImg from '../styles/image/home/banner-6.png';
+
 
 export const Home = () => {
     return (
         <>
-            <h2>Home</h2>
+            <div className="pd-home-wrapper">
+                <div className="simon-sparks-bg">
+                    <img src={HomeBannerImg} className="pd-home-wrapper__banner"/>
+                </div>
+            </div>
         </>
     )
 }
