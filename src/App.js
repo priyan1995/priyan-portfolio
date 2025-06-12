3331e3e2fc1154a5a9698799b8ed3547cb812723
priyan1995 <priyan.abewickrama@ideahub.lk>
2025-06-12 18:58:07 +0530

    file sctructuring

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
+import '../src/styles/style.scss';
+
 
 function App() {
   return (
